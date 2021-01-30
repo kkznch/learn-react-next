8f1c27a8aacdbca22ee94a02c3832053c998ae55
kkznch <kkznch710md@gmail.com>
2021-01-30 15:07:34 +0900

    入力フォーム作った

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
+import { useForm, NestedValue } from 'react-hook-form';
+import { Input } from '../components/Todo/Input';
 import { List } from '../components/Todo/List';
 
+type FormData = {
+    todo: string;
+};
+
 export default function Home() {
+  const { register, handleSubmit, reset } = useForm<FormData>({
+    defaultValues: {
+      todo: '',
+    },
+  });
+  const onSubmit = handleSubmit(({todo}) => {
+    console.log('submit: ', todo);
+    reset();
+  })
+
   return (
     <div>
-        <List />
+      <form onSubmit={onSubmit}>
+        <Input name="todo" ref={register} />
+      </form>
+      <List />
     </div>
   )
 }
